7c59eb7663eddeb5bdcd2c4bc677b05fed1fa784
Nguyễn Thành Tín <88455780+TomTom2k@users.noreply.github.com>
2023-11-05 22:26:49 +0700

    erge interface

--- src/ptud/Connection/ConnectDB.java
+++ src/ptud/Connection/ConnectDB.java
@@ -13,35 +13,35 @@ public class ConnectDB {
     private static java.sql.Connection connection = null;
     
     //Constructor
-	public ConnectDB() {}
-	
-	//Get
-	public static ConnectDB getInstance() {return instance;}
-	public static java.sql.Connection getConnection() {return connection;}
+    public ConnectDB() {}
+
+    //Get
+    public static ConnectDB getInstance() {return instance;}
+    public static java.sql.Connection getConnection() {return connection;}
     //Methods
-	public void connectDatabase() {
-		try {
-			//Login by Windows Authentication
-				//String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelManagement;integratedSecurity=true;";
-			
-			//Login by SQL Login
-				String url = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyLSP";
-				String username = "sa";
-				String password = "12345678";
-				
-			connection = java.sql.DriverManager.getConnection(url, username, password);
-		} 
-		catch (java.sql.SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	public void disconnectDatabase() {
-		if(connection != null) {
-			try {
-				connection.close();
-			} catch (java.sql.SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public void connectDatabase() {
+        try {
+            //Login by Windows Authentication
+//            String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelManagement;integratedSecurity=true;";
+
+            //Login by SQL Login
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyLSP";
+            String username = "sa";
+            String password = "12345678";
+
+            connection = java.sql.DriverManager.getConnection(url, username, password);
+        } 
+        catch (java.sql.SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void disconnectDatabase() {
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (java.sql.SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
